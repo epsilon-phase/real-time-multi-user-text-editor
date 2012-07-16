c1ce35f9824e9a6d8988287a9be44f0a9e295b94
awhite@ducomputingacademy.org <awhite@ducomputingacademy.org@30c9b04c-ffb7-da41-b5c1-2b98f6cab434>
2012-07-16 17:23:16 +0000

    Fixed issues with string manipulation.

---
+++ RealServer/RealServer/TestProject1/TextTransformTest.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OperationalTransform;
+namespace TestProject1
+{
+    [TestClass]
+    public class TextTransformTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TextTransformCollection y = new TextTransformCollection("Hi");
+            y.add(new TextTransformActor(2,"Hi There",DateTime.Now));
+            Assert.AreEqual("HiHi There",y.CalculateConsolidatedString());
+            //delete from it
+            y.add(new TextTransformActor(0, 2, DateTime.Now));
+            Assert.AreEqual("Hi There", y.CalculateConsolidatedString());
+            y.add(new TextTransformActor(6, " You person", DateTime.Now));
+            Assert.AreEqual("Hi There You person", y.consolidated);
+        }
+    }
+}
