38a4f475d199760d3fb157d10c7235b0ffb14425
thighfill@ducomputingacademy.org <thighfill@ducomputingacademy.org@30c9b04c-ffb7-da41-b5c1-2b98f6cab434>
2012-07-24 19:46:31 +0000

    Made a small adjustment on field types in CodeLangLib, deciding to make static instances of the classes rather than static classes.

--- branches/alexversion/RealServer/RealServer/CodeLangLib/Java.cs
+++ branches/alexversion/RealServer/RealServer/CodeLangLib/Java.cs
@@ -10,14 +10,14 @@ namespace CodeLangLib
     class Java:Language
     {
         internal Java(){
-            Java.keywords = "abstract,continue,for,new,switch,assert,default,goto,package,synchronized,boolean,do,if,private,this,break,double,implements,protected,throw,byte,else,import,public,throws,case,enum,instanceof,return,transient,catch,extends,int,short,try,char,final,interface,static,void,class,finally,long,strictfp,volatile,const,float,native,super,while".Split(',');
-            Java.fileExtension = ".java";
-            Java.commentChar = "//";
-            Java.supportsMultiLineComments = true;
-            Java.multiLineCommentStart = "/*";
-            Java.multiLineCommentEnd = "*/";
-            Java.keywordColor = Color.Purple;
-            Java.stringColor = Color.Blue;
+            this.keywords = "abstract,continue,for,new,switch,assert,default,goto,package,synchronized,boolean,do,if,private,this,break,double,implements,protected,throw,byte,else,import,public,throws,case,enum,instanceof,return,transient,catch,extends,int,short,try,char,final,interface,static,void,class,finally,long,strictfp,volatile,const,float,native,super,while".Split(',');
+            this.fileExtension = ".java";
+            this.commentChar = "//";
+            this.supportsMultiLineComments = true;
+            this.multiLineCommentStart = "/*";
+            this.multiLineCommentEnd = "*/";
+            this.keywordColor = Color.Purple;
+            this.stringColor = Color.Blue;
         }
     }
 }
