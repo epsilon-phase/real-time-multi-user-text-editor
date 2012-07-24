287db55dd5d1cd1bc593a88a8b43a88f5c9983c5
thighfill@ducomputingacademy.org <thighfill@ducomputingacademy.org@30c9b04c-ffb7-da41-b5c1-2b98f6cab434>
2012-07-24 19:26:20 +0000

    Added class library project CodeLangLib to be used for text formatting purposes. Currently supports C, C++, C#, VB.NET, Java, and Python.

---
+++ branches/alexversion/RealServer/RealServer/CodeLangLib/Java.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace CodeLangLib
+{
+    class Java:Language
+    {
+        internal Java(){
+            Java.keywords = "abstract,continue,for,new,switch,assert,default,goto,package,synchronized,boolean,do,if,private,this,break,double,implements,protected,throw,byte,else,import,public,throws,case,enum,instanceof,return,transient,catch,extends,int,short,try,char,final,interface,static,void,class,finally,long,strictfp,volatile,const,float,native,super,while".Split(',');
+            Java.fileExtension = ".java";
+            Java.commentChar = "//";
+            Java.supportsMultiLineComments = true;
+            Java.multiLineCommentStart = "/*";
+            Java.multiLineCommentEnd = "*/";
+            Java.keywordColor = Color.Purple;
+            Java.stringColor = Color.Blue;
+        }
+    }
+}
