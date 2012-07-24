c778ae02ff43cce9705dada5372ba9c196b9aa30
thighfill@ducomputingacademy.org <thighfill@ducomputingacademy.org@30c9b04c-ffb7-da41-b5c1-2b98f6cab434>
2012-07-24 19:26:20 +0000

    Added class library project CodeLangLib to be used for text formatting purposes. Currently supports C, C++, C#, VB.NET, Java, and Python.

---
+++ RealServer/RealServer/CodeLangLib/C.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeLangLib
+{
+    class C:Language
+    {
+        internal C(){
+            C.keywords = "auto,break,case,char,const,continue,default,do,double,else,enum,extern,float,for,goto,if,int,long,register,return,short,signed,sizeof,static,struct,switch,typedef,union,unsigned,void,volatile,while".Split(',');
+            C.commentChar = "//";
+            C.multiLineCommentStart = "/*";
+            C.multiLineCommentEnd = "*/";
+            C.supportsMultiLineComments = true;
+            C.fileExtension = ".c";
+        }
+    }
+}
---
+++ RealServer/RealServer/CodeLangLib/Language.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace CodeLangLib
+{
+    public class Language
+    {
+        public static string[] keywords;
+        public static string commentChar;
+        public static string multiLineCommentStart;
+        public static string multiLineCommentEnd;
+        public static bool supportsMultiLineComments = false;
+        public static string fileExtension = "";
+        public static Color keywordColor = Color.Blue;
+        public static Color stringColor = Color.Salmon;
+        public static Color commentColor = Color.LightGreen;
+        public static Color numberColor = Color.Black;
+
+        public static readonly Language LangC = new C();
+        public static readonly Language LangCSharp = new CSharp();
+        public static readonly Language LangCPlusPlus = new CPlusPlus();
+        public static readonly Language LangPython = new Python();
+        public static readonly Language LangJava = new Java();
+    }
+}
